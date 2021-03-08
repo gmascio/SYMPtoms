71dbb40b790ad3e4122f32d1503bbe9cf11e3a0a
Gigi <gigi@Gigis-MacBook-Pro.local>
2021-03-08 17:21:00 -0500

    Home page rendering WIP

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import Illnesses from '../screens/Illnesses';
-import { useState, useEffect, Route } from 'react-router-dom';
-import getAllIllnesses from '../services/illnesses'
+import { useState, useEffect} from 'react';
+import { Route ,  useHistory  } from 'react-router-dom';
+import { getAllIllnesses } from '../services/illnesses';
+import { destroySymptom, getAllSymptoms, postSymptom, putSymptom } from '../services/symptoms';
 
 function MainContainer() {
   const [illnesses, setIllnesses] = useState([]);
+  const [symptoms, setSymptoms] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchIllnesses = async () => {
       const illnessesList = await getAllIllnesses();
       setIllnesses(illnessesList);
     }
-    fetchFoods();
+    fetchIllnesses();
   }, [])
+  useEffect(() => {
+    const fetchSymptoms = async () => {
+      const symptomsList = await getAllSymptoms();
+      setSymptoms(symptomsList);
+    }
+    fetchSymptoms();
+  }, [])
+
+  const handleCreate = async (formData) => {
+    const newSymptom = await postSymptom(formData);
+    setSymptoms(prevState => [...prevState, newSymptom]);
+    history.push('/');
+  }
+
+  const handleDelete = async (id) => {
+    await destroySymptom(id);
+    setSymptoms(prevState => prevState.filter((symptom) => symptom.id !== id))
+  }
+
+  const handleUpdate = async (id, formData) => {
+    const updatedSymptom = await putSymptom(id, formData);
+    setSymptoms(prevState => prevState.map((symptom) => {
+      return symptom.id === Number(id) ? updatedSymptom : symptom
+    }));
+    history.push('/');
+  }
   return (
     <div>
       <Route path='/'>
--- client/src/screens/Illnesses.jsx
+++ client/src/screens/Illnesses.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 
-function Illnesses() {
+function Illnesses(props) {
+  const {illnesses} = props
   return (
     <div>
-      
+      <h3>
+        Illnesses
+      </h3>
+      {illnesses.map((illness) => (
+        <React.Fragment key={illness.id}>
+          <Link to={`/illnesses/${illness.id}`}><p>{illness.name}</p></Link>
+         
+        </React.Fragment>
+      ))}
     </div>
   )
 }
