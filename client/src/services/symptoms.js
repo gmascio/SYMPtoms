1e5eca0936635855660e32624dadd7a620631746
Gigi <gigi@Gigis-MacBook-Pro.local>
2021-03-08 01:05:44 -0500

    Service folder additions

---
+++ client/src/services/symptoms.js
@@ -0,0 +1,26 @@
+import api from './api-config';
+
+export const getAllIllnesses = async () => {
+  const resp = await api.get('/illnesses');
+  return resp.data;
+}
+
+export const getOneIllness = async (id) => {
+  const resp = await api.get(`/illnesses/${id}`);
+  return resp.data;
+}
+
+export const postIllness = async (illnessData) => {
+  const resp = await api.post('/Illnesses', { illness: llnesseData });
+  return resp.data;
+}
+
+export const putIllness = async (id, illnessData) => {
+  const resp = await api.put(`/illnesses/${id}`, { illness: illnessData });
+  return resp.data;
+}
+
+export const destroyIllness = async (id) => {
+  const resp = await api.delete(`/illnesses/${id}`);
+  return resp;
+}
